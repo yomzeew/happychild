4cd740b4d06ccfdb1c780116b2d0596547760eb6
yomzeew <yomzeew@gmail.com>
2024-07-14 19:46:58 +0100

    another commit

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -1,5 +1,10 @@
 import { useState } from "react"
 import logopng from './images/logopng.png'
+import dashbaordicon from './images/dashboardicon.svg'
+import profileicon from './images/profileicon.svg'
+import logouticon from './images/logouticon.svg'
+import websiteicon from './images/websiteicon.svg'
+import calendaricon from './images/calendaricon.svg'
 
 const Dashboard=()=>{
     const [onchangewidth,setonchangewidth]=useState(false)
@@ -13,21 +18,73 @@ const Dashboard=()=>{
             <div>
                 <div className="pt-5">
                     <img src={logopng} className="w-16 h-auto object-fit"/>
-                    <div className='potta-one-regular md:text-sm text-bluecolor text-xs'>HappyChild</div>
+                <div className='potta-one-regular md:text-sm text-bluecolor text-xs'>HappyChild</div>
                 </div>
+                <div className="mt-10">
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg ">
+                    <img src={dashbaordicon} />
+                    <div className="fredoka md:text-sm text-bluecolor text-xs">Dashboard</div>
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg mt-3">
+                    <img src={profileicon} />
+                    <div className="fredoka md:text-sm text-bluecolor text-xs">Profile</div>
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg  mt-3">
+                    <img src={calendaricon} />
+                    <div className="fredoka md:text-sm text-bluecolor text-xs">Appointment Schedule</div>
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg mt-3">
+                    <img src={websiteicon} />
+                    <div className="fredoka md:text-sm text-bluecolor text-xs">Back to Company Website</div>
+                    
+                </div>
+
+                </div>
+              
                 
             </div>
 
             </div>
-            <div  onClick={handlewidth} className={`md:hidden block ${onchangewidth?'w-44':'w-16'} bg-slate-100 h-screen px-3 cursor-s-resize`}>
+            <div className={`md:hidden block ${onchangewidth?'w-56':'w-16'} bg-slate-100 h-screen px-3 cursor-s-resize`}>
+                <div onClick={handlewidth} className={`absolute top-1/2  ${onchangewidth?'left-52':'left-12'} `}><i className={`text-slate-400 opacity-70 fa fa-2x ${onchangewidth?'fa-arrow-circle-o-left':'fa-arrow-circle-o-right'}  `}></i></div>
             <div>
-            <div className="flex gap-3 pt-3">
-                <i className="fa text-bluecolor fa-bars"></i>
+            <div className="flex items-center gap-3 pt-5">
+                {!onchangewidth &&<i onClick={handlewidth} className="fa text-bluecolor fa-bars"></i>}
                 {onchangewidth &&<div>
                     <img src={logopng} className="w-12 h-auto object-fit"/>
                     <div className='potta-one-regular md:text-sm text-bluecolor text-xs'>HappyChild</div>
                 </div>}
+                
+
             </div>
+            <div className="pt-5">     
+         
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg ">
+                    <img src={dashbaordicon} />
+                    {onchangewidth &&<div className="fredoka md:text-sm text-bluecolor text-xs">Dashboard</div>}
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg mt-3">
+                    <img src={profileicon} />
+                    {onchangewidth &&<div className="fredoka md:text-sm text-bluecolor text-xs">Profile</div>}
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg  mt-3">
+                    <img src={calendaricon} />
+                    {onchangewidth &&<div className="fredoka md:text-sm text-bluecolor text-xs">Appointment Schedule</div>}
+                    
+                </div>
+                <div className="flex gap-3 items-center bg-slate-300 px-3 py-3 rounded-lg mt-3">
+                    <img src={websiteicon} />
+                    {onchangewidth &&<div className="fredoka md:text-sm text-bluecolor text-xs">Back to Company Website</div>}
+                    
+                </div>
+
+                </div>
+               
            
 
             </div>
