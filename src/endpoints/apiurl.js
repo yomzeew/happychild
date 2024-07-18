908a944607e2103a280561ae7bcc485895208286
yomzeew <yomzeew@gmail.com>
2024-07-18 19:42:00 +0100

    new update

--- src/endpoints/apiurl.js
+++ src/endpoints/apiurl.js
@@ -1,5 +1,5 @@
-const BaseURi='http://localhost:3300/api/v1'
-// const BaseURi='https://happychildbackend.onrender.com/api/v1'
+// const BaseURi='http://localhost:3300/api/v1'
+const BaseURi='https://happychildbackend.onrender.com/api/v1'
 export const loginurl=`${BaseURi}/login`
 export const register=`${BaseURi}/register`
 export const update=`${BaseURi}/update`
